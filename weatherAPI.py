d3b23958054a0a72a5c40d37e1fc1e9fa93702d7
Tatiana <tstwrt7@gmail.com>
2024-03-14 16:13:49 -0400

    Added user input for city to weatherAPI

--- weatherAPI.py
+++ weatherAPI.py
@@ -2,14 +2,17 @@
 
 import requests
 
-weather = requests.get('http://api.weatherapi.com/v1/current.json?key=7f1bbdbd3ebb4f98a93194516241403&q=Atlanta&aqi=no')
+city = input('What city would you like the weather for?\n')
+
+weather = requests.get('http://api.weatherapi.com/v1/current.json?key=7f1bbdbd3ebb4f98a93194516241403&q=' + city + '&aqi=no')
 json = weather.json()
 
 city = json['location']['name']
-state = json['location']['region']
+region = json['location']['region']
+country = json['location']['country']
 
 tempInF = json['current']['temp_f']
 condition = json['current']['condition']['text']
 
-print('In', city + ',', state, 'it is currently', str(tempInF) + '°F')
-print('The condition is', condition)
+print('In', city + ',', region + ',', country, 'it is currently', str(tempInF) + '°F')
+print('The condition is', condition)    
